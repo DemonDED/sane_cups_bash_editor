5b2e851e3350482540b806e0bd8b47d7ef730a32
DemonDED <DemonDED@inbox.ru>
2026-06-05 14:48:07 +0700

    initialize script, add root and programm status check

---
+++ scbe.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Main script
+
+echo -e 'Welcome to Sane-Cups Bash Editor (scbe)!\n'
+
+if [ "$EUID" -eq 0 ]; then
+	echo "Hello Administrator!"
+else
+	echo "[ERROR]: This command must work on root!"
+	exit 1
+fi
+
+if ! command -v cups &> /dev/null; then
+	echo "[ERROR]: CUPS do not install on system"
+	echo "[WARNING]: Please, install CUPS and restart command"
+	exit 1
+fi
+
+if ! command -v scanimage &> /dev/null; then
+	echo "[ERROR]: SANE do not install on system"
+	echo "[WARNING]: Please, install SANE and restart command"
+fi
