4d82e38ba9cfc1f33b3da9eaf600ba5049f9117b
DemonDED <DemonDED@inbox.ru>
2026-06-05 15:24:27 +0700

    fix prototype, work alpha0.1

--- scbe.sh
+++ scbe.sh
@@ -3,8 +3,8 @@
 # Main variables
 
 PATH_TO_AIRSCAN="/etc/sane.d/airscan.conf"
-SCANNER_IP
-SCANNER_NAME
+SCANNER_IP=""
+SCANNER_NAME=""
 
 # Main script
 
@@ -19,21 +19,19 @@ else
 	exit 1
 fi
 
-if ! command -v cups &> /dev/null; then
-	echo "[ERROR]: CUPS do not install on system"
-	echo "[WARNING]: Please, install CUPS and restart command"
-	exit 1
-fi
+#if ! command -v cups &> /dev/null; then
+#	echo "[ERROR]: CUPS do not install on system"
+#	echo "[WARNING]: Please, install CUPS and restart command"
+#	exit 1
+#fi
 
-if ! command -v scanimage &> /dev/null; then
-	echo "[ERROR]: SANE do not install on system"
-	echo "[WARNING]: Please, install SANE and restart command"
-fi
+#if ! command -v scanimage &> /dev/null; then
+#	echo "[ERROR]: SANE do not install on system"
+#	echo "[WARNING]: Please, install SANE and restart command"
+#fi
 
-read -p "Enter IP to airscan add: "
-SCANNER_IP
+read -p "Enter IP to airscan add: " SCANNER_IP
 
-read -p "Enter Name device: "
-SCANNER_NAME
+read -p "Enter Name device: " SCANNER_NAME
 
-echo "'$SCANNER_NAME' = http://$SCANNER_IP:631/eSCL, escl" >> "$PATH_TO_AIRSCAN"
+sed -i "/^\[devices\]$/a '$SCANNER_NAME' = http://$SCANNER_IP:631/eSCL, escl" $PATH_TO_AIRSCAN
