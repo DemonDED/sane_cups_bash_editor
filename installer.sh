8d25a0cf0bff375d7714e5004fd26b0c319185e1
DemonDED <DemonDED@inbox.ru>
2026-06-09 11:30:07 +0700

    fix installer target file

--- installer.sh
+++ installer.sh
@@ -4,17 +4,19 @@
 set -e
 
 # Main variables
-TARGET_DIR=/usr/local/sbin/scbe
-BINARY_NAME="Sane-Cups Bash Editor"
+TARGET_DIR=/usr/local/sbin
+BINARY_NAME="scbe"
+PROGRAM_NAME="Sane-Cups Bash Editor"
+LINK_MAIN_BINARY="https://raw.githubusercontent.com/DemonDED/sane_cups_bash_editor/refs/heads/develope/scbe.sh"
 
-echo "Download and install ${BINARY_NAME} by DemonDED"
+echo "Download and install ${PROGRAM_NAME} by DemonDED"
 
 if [[ "$EUID" -ne 0 ]]; then
 	echo "This install must work on root!"
 	exit 1
 fi
 
-curl -sSL "https://raw.githubusercontent.com/DemonDED/sane_cups_bash_editor/refs/heads/develope/installer.sh" -o "$TARGET_DIR/$BINARY_NAME"
+curl -sSL "$LINK_MAIN_BINARY" -o "$TARGET_DIR/$BINARY_NAME"
 
 chmod +x "$TARGET_DIR/$BINARY_NAME"
 
