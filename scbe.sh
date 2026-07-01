dc3cf90770107ca0e28af903bece300ee99b1f52
DemonDED <DemonDED@inbox.ru>
2026-07-01 17:08:52 +0700

    [develop]: add fucntions for menu, add check for setup menu

--- scbe.sh
+++ scbe.sh
@@ -18,13 +18,52 @@ ACTION_FOR_CUPS=""
 GREEN="\e[32m"
 RED="\e[31m"
 YELLOW="\e[33m"
+ORANGE="\e[38;5;208m"
 RESET_COLOR="\e[0m"
-
 
 ERROR_MSG="$RED[ERROR]$RESET_COLOR"
 WARNING_MSG="$YELLOW[WARNING]$RESET_COLOR"
 
 #Main funcitons
+setup_menu() {
+	echo -e "${GREEN}1.$RESET_COLOR Set SANE config" >&2
+	echo -e "${GREEN}2.$RESET_COLOR Set ${ORANGE}CUPS$RESET_COLOR config" >&2
+	echo -e "${GREEN}3.$RESET_COLOR Optimization SANE airscan (auto scan ip off)" >&2
+	echo -e "${GREEN}4.$RESET_COLOR Optimization CUPS (browsed off)" >&2
+}
+cups_menu() {
+	echo -e "${ORANGE}1.$RESET_COLOR Add new device"
+	echo -e "${ORANGE}2.$RESET_COLOR Edit exist device"
+	echo -e "${ORANGE}3.$RESET_COLOR Delete exist device"
+	echo -e "${ORANGE}4.$RESET_COLOR Show devices list"
+}
+
+get_setup() {
+	local setup_local=""
+
+	setup_menu
+
+	while true; do
+		read -p "Please, choose setup work: " setup_local
+		
+		if check_setup "$setup_local"; then
+			echo "$setup_local"
+			return 0
+		fi
+	done
+}
+
+check_setup() {
+	local set_loc=$1
+
+	if [[ $set_loc -ne 1 && $set_loc -ne 2 && $set_loc -ne 3 && $set_loc -ne 4 ]]; then
+		echo -e "$ERROR_MSG: Incorrect setup value!" >&2
+		echo -e "$WARNING_MSG: Please, enter correct setup value!" >&2
+		return 1
+	else
+		return 0
+	fi
+}
 
 get_ip_addr() {
 	local text_local=$1
@@ -66,12 +105,7 @@ else
 	exit 1
 fi
 
-echo -e "${GREEN}1.$RESET_COLOR Set SANE config"
-echo -e "${GREEN}2.$RESET_COLOR Set CUPS config"
-echo -e "${GREEN}3.$RESET_COLOR Optimization SANE airscan (auto scan ip off)"
-echo -e "${GREEN}4.$RESET_COLOR Optimization CUPS (browsed off)"
-
-read -p "Please, choose setup work: " SETUP
+SETUP=$(get_setup)
 
 # Enter for SANE scanner data
 if [[ $SETUP -eq 1 ]]; then
@@ -104,8 +138,8 @@ if [[ $SETUP -eq 2 ]]; then
 
 	if command -v lpadmin >/dev/null 2>&1; then
 
-		echo -e "1. Add new device\n2. Edit exist device\n3. Delete exist device\n4. Show devices list\n"
-		
+		cups_menu
+
 		read -p "Choose action: " ACTION_FOR_CUPS
 
 		CUPS_DATA_DEVICES=$(lpstat -v 2>&1)
@@ -172,3 +206,4 @@ if [[ $SETUP -eq 4 ]]; then
 	systemctl stop cups-browsed
 	systemctl disable cups-browsed
 fi
+	
