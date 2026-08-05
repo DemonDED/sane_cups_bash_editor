8a2da6801f36df588cd0c4516e39c98c54e567c8
DemonDED <DemonDED@inbox.ru>
2026-08-05 15:35:30 +0700

    [refactor]: modulatize sane functional for script and include to navigation functional

--- scbe.sh
+++ scbe.sh
@@ -3,6 +3,7 @@
 source ./variables_scbe.sh
 source ./colors_scbe.sh
 source ./functions_scbe.sh
+source ./sane_functional_scbe.sh
 
 #loader_dot() {
 
@@ -12,7 +13,7 @@ source ./functions_scbe.sh
 
 echo -e 'Welcome to Sane-Cups Bash Editor (scbe)!\n'
 
-## Status check
+### Status check ###########
 
 if [ "$EUID" -eq 0 ]; then
 	echo "Hello Administrator!"
@@ -22,96 +23,101 @@ else
 	exit 1
 fi
 
+############################
+
+### Main while cycle #######
+
 while [ $MAIN_FLAG -eq 1 ]; do
 
-SETUP=$(get_setup)
+	SETUP=$(get_setup)
 
-# Enter for SANE scanner data
-if [[ $SETUP -eq 1 ]]; then
+	# Enter for SANE scanner data
+	if [[ $SETUP -eq 1 ]]; then
 
-# while [ ]
+	SANE_FLAG=1
+	echo -e "\n"
 
-	if [[ -f $PATH_TO_AIRSCAN ]]; then
+	while [ $SANE_FLAG -eq 1 ]; do
 
-		SCANNER_IP=$(get_ip_addr "Enter ip for airscan")
+		if [[ -f $PATH_TO_AIRSCAN ]]; then
 
-		read -p "Enter Name device: " SCANNER_NAME
-		read -p "Enter scaner port (if applicable) or press Enter to skip: " SCANNER_PORT
+			SANE_SETUP=$(get_sane_setup)
+			echo -e "\n"
 
-		if [[ ${SCANNER_PORT} -ne "" ]]; then
-			SCANNER_PORT=":$SCANNER_PORT"
+			if [[ $SANE_SETUP -eq 1 ]]; then
+				sane_add_new_device
+			fi
+			if [[ $SANE_SETUP -eq 2 ]]; then
+				sane_device_list
+			fi
+			if [[ $SANE_SETUP -eq 3 ]]; then
+				sane_back_to_main_menu
+			fi
+
+		else
+
+			echo -e "$ERROR_MSG: This setup work only with sane-airscan backend!"
+			echo -e "$WARNING_MSG: Please, install sane-airscan - sudo apt install sane-airscan\n"
+			SANE_FLAG=0
+			exit 1
+
 		fi
-
-
-		sed -i "/^\[devices\]$/a '$SCANNER_NAME' = http://$SCANNER_IP${SCANNER_PORT}/eSCL, escl" $PATH_TO_AIRSCAN
-
-	else
-
-		echo -e "$ERROR_MSG: This setup work only with sane-airscan backend!"
-		echo -e "$WARNING_MSG: Please, install sane-airscan - sudo apt install
-		sane-airscan"
-		exit 1
-
+	done
 	fi
-fi
-# done
+
+	if [[ $SETUP -eq 2 ]]; then
+
+		if command -v lpadmin >/dev/null 2>&1; then
+
+			cups_menu
+
+			read -p "Choose action: " ACTION_FOR_CUPS
+
+			CUPS_DATA_DEVICES=$(lpstat -v 2>&1)
+
+			if [[ $ACTION_FOR_CUPS -eq 4 ]]; then
+				echo -e "\nExist data of devices:\n"
+				echo "$CUPS_DATA_DEVICES"
+			fi
+
+			#lpstat -v
+			if [[ $ACTION_FOR_CUPS -eq 1 ]]; then
+				NAME_NEW_CUPS_DEVICE=""
+				IP_NEW_CUPS_DEVICE=""
+				DESCRIPTION_NEW_CUPS_DEVICE=""
+				LOCATION_NEW_CUPS_DEVICE=""
+
+				#lpadmin -p "My name" -E (activate) -v socket://192.168.229.***:9100
+				#-m everywhere -D описание опционально -L расположение опционально
+				read -p "Enter name new device: " NAME_NEW_CUPS_DEVICE
+
+				#read -p "Enter ip new device: " IP_NEW_CUPS_DEVICE
+				IP_NEW_CUPS_DEVICE=$(get_ip_addr "Enter ip for new device CUPS")
 
 
-if [[ $SETUP -eq 2 ]]; then
+				read -p "Enter description (if need): " DESCRIPTION_NEW_CUPS_DEVICE
+				read -p "Enter location (if need): " LOCATION_NEW_CUPS_DEVICE
 
-	if command -v lpadmin >/dev/null 2>&1; then
 
-		cups_menu
+				lpadmin -p "$NAME_NEW_CUPS_DEVICE" -E \
+				-v ipp://$IP_NEW_CUPS_DEVICE:9100 \
+				-m everywhere \
+				-D "$DESCRIPTION_NEW_CUPS_DEVICE" \
+				-L "$LOCATION_NEW_CUPS_DEVICE"
+			fi
+			if [[ $ACTION_FOR_CUPS -eq 2 ]]; then
+				echo "You choose 2"
+			fi
+			if [[ $ACTION_FOR_CUPS -eq 3 ]]; then
+				echo "You choose 3"
+			fi
 
-		read -p "Choose action: " ACTION_FOR_CUPS
+		else
+			echo -e "$ERROR_MSG: This setup work only with cups utility"
+			echo -e "$WARNING_MSG: Please, install cups and cups-client - sudo apt install cups cups-client"
 
-		CUPS_DATA_DEVICES=$(lpstat -v 2>&1)
-
-		if [[ $ACTION_FOR_CUPS -eq 4 ]]; then
-			echo -e "\nExist data of devices:\n"
-			echo "$CUPS_DATA_DEVICES"
 		fi
-
-		#lpstat -v
-		if [[ $ACTION_FOR_CUPS -eq 1 ]]; then
-			NAME_NEW_CUPS_DEVICE=""
-			IP_NEW_CUPS_DEVICE=""
-			DESCRIPTION_NEW_CUPS_DEVICE=""
-			LOCATION_NEW_CUPS_DEVICE=""
-
-			#lpadmin -p "My name" -E (activate) -v socket://192.168.229.***:9100
-			#-m everywhere -D описание опционально -L расположение опционально
-			read -p "Enter name new device: " NAME_NEW_CUPS_DEVICE
-			
-
-			#read -p "Enter ip new device: " IP_NEW_CUPS_DEVICE
-			IP_NEW_CUPS_DEVICE=$(get_ip_addr "Enter ip for new device CUPS")
-
-
-			read -p "Enter description (if need): " DESCRIPTION_NEW_CUPS_DEVICE
-			read -p "Enter location (if need): " LOCATION_NEW_CUPS_DEVICE
-
-
-			lpadmin -p "$NAME_NEW_CUPS_DEVICE" -E \
-			-v ipp://$IP_NEW_CUPS_DEVICE:9100 \
-			-m everywhere \
-			-D "$DESCRIPTION_NEW_CUPS_DEVICE" \
-			-L "$LOCATION_NEW_CUPS_DEVICE"
-		fi
-		if [[ $ACTION_FOR_CUPS -eq 2 ]]; then
-			echo "You choose 2"
-		fi
-		if [[ $ACTION_FOR_CUPS -eq 3 ]]; then
-			echo "You choose 3"
-		fi
-
-	else
-		echo -e "$ERROR_MSG: This setup work only with cups utility"
-		echo -e "$WARNING_MSG: Please, install cups and cups-client - sudo apt install
-		cups cups-client"
-
 	fi
-fi
 
 # Optimization for airscan (without auto scaning ip)
 if [[ $SETUP -eq 3 ]]; then
@@ -137,6 +143,7 @@ fi
 
 if [[ $SETUP -eq 5 ]]; then
 	MAIN_FLAG=0
+	echo -e "\n"
 fi
 
 done
