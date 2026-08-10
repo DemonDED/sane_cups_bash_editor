1bcfee630abf23cb7785a3bc09d800b8641b8150
DemonDED <DemonDED@inbox.ru>
2026-08-10 13:40:16 +0700

    [develope]: new installer in work

--- installer.sh
+++ installer.sh
@@ -1,24 +1,67 @@
 #!/bin/bash
 
-# Down if error
-set -e
+#Variables
 
-# Main variables
-TARGET_DIR=/usr/local/sbin
+TARGET_BIN_DIR=/usr/local/bin
+TARGET_LIB_DIR=/usr/local/lib
+
 BINARY_NAME="scbe"
 PROGRAM_NAME="Sane-Cups Bash Editor"
-LINK_MAIN_BINARY="https://raw.githubusercontent.com/DemonDED/sane_cups_bash_editor/refs/heads/develope/scbe.sh"
 
-echo "Download and install ${PROGRAM_NAME} by DemonDED"
+SETUP=""
+
+LINK_MAIN_BINARY="https://github.com/DemonDED/sane_cups_bash_editor/archive/refs/heads/master.zip"
+LINK_DEV_BUILD="https://github.com/DemonDED/sane_cups_bash_editor/archive/refs/heads/develope.zip"
+
+#Main script
+
+echo -e "Download and install ${PROGRAM_NAME} by DemonDED\n"
 
 if [[ "$EUID" -ne 0 ]]; then
-	echo "This install must work on root!"
+	echo "This install must work in root!"
 	exit 1
 fi
 
-curl -sSL "$LINK_MAIN_BINARY" -o "$TARGET_DIR/$BINARY_NAME"
+while true; do
 
-chmod +x "$TARGET_DIR/$BINARY_NAME"
+	echo -e "Welcome! Please choose:"
+	echo -e "1. Install stable version"
+	echo -e "2. Install develope build"
 
-echo "Install complete!"
-echo "You must use programm with command: scbe"
+	read -p "" SETUP
+
+	if [[ "$SETUP" -eq 1 ]]; then
+		curl -L -O "$LINK_MAIN_BINARY"
+		
+		unzip master.zip
+		cd ./sane_cups_bash_editor-master
+		cp scbe.sh "$TARGET_BIN_DIR/$BINARY_NAME"
+
+		cp *.sh
+
+		chmod +x "$TARGET_DIR/$BINARY_NAME"
+
+		echo "Install complete!"
+		echo "You must use programm with command: scbe"
+
+		return 0
+	fi
+
+	if [[ "$SETUP" -eq 2 ]]; then
+		curl -L -O "$LINK_DEV_BUILD"
+
+		unzip develope.zip
+		cd ./sane_cups_bash_editor-develope
+		cp scbe.sh "$TARGET_BIN_DIR/$BINARY_NAME"
+
+		cp *.sh
+
+		chmod +x "$TARGET_DIR/$BINARY_NAME"
+
+		echo "Install complete!"
+		echo "You must use programm with command: scbe"
+
+		return 0
+	fi
+
+done
